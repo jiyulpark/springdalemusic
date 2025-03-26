81f843dc7534cd609b9c93684ae8e114e7dbd620
jiyulpark <funlove0268@naver.com>
2025-03-26 16:42:27 +0900

    GA제거

--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,11 @@
 // pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document';
 
-const isProd = process.env.NODE_ENV === 'production';
-
 export default function Document() {
   return (
     <Html lang="ko">
       <Head>
-        {isProd && (
-          <>
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-YZP7L66RXP"></script>
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){ dataLayer.push(arguments); }
-                  gtag('js', new Date());
-                  gtag('config', 'G-YZP7L66RXP', { send_page_view: false });
-                `,
-              }}
-            />
-          </>
-        )}
+        {/* ✅ GA 코드 완전 제거됨 */}
       </Head>
       <body>
         <Main />
