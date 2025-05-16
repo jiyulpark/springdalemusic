cf6d77397dd1e38bb2e5a046ed1602900db79ecb
jiyulpark <funlove0268@naver.com>
2025-05-16 13:40:47 +0900

    환경변수명을 SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY로 수정

--- pages/api/account/delete.js
+++ pages/api/account/delete.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 export const config = { api: { bodyParser: true } };
 
 const admin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
